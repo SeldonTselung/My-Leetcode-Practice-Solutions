a0c8c24d2255b9a71db5b7f89884bd050d1d7599
Seldon Tselung <stselung@gmail.com>
2022-12-01 19:44:43 -0500

    /* Given an m x n matrix, return all elements of the matrix in spiral order.
        Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
        Output: [1,2,3,6,9,8,7,4,5]
    
        Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
        Output: [1,2,3,4,8,12,11,10,9,5,6,7]
    
        Constraints:
          m == matrix.length
          n == matrix[i].length
          1 <= m, n <= 10
          -100 <= matrix[i][j] <= 100
    */
    
    /* =========================== MY APPROACH ===========================
    
    Since it's a matrix, I can assume it has no more than 4 corners.
    Since it's asking me to go in spiral order, I will assume the direction to be clockwise. That means it's going right, down, left, up.
    
    So I need my code to read the input in right, down, left, up directions on a loop until it has reached the end.
    
    Initializing some boundaries such rowMin = 0 and colMin = 0,rowMax = m - 1, colMax = n - 1 will help me shrink the size of the
    matrix so I can continue to spiral inwards.
    
    right ---> staying at rowMin, iterate from colMin - colMax (rowMin++)
    down --->  staying at colMax, iterate from rowMin - rowMax (colMax--)
    left --->  staying at rowMax, iterate from colMax - colMin (rowMax--)
    up --->  staying at colMin, iterate from rowMax - rowMin (colMin++)
    
    How will I know that I have hit the end of the spiral?
      - Well m * n gives us the exact number of elements that should be there in this matrix. So as we spiral inwards pushing elements into our result, we can check if the length of the result = size of the matrix.
      - So our stopping condition would be when the length of the result is equal to the size of the matrix.
    
    What if my array is []?
      - I can return null or anything that is required of that edge case
    
    Will this work for single row or column?
      - It should because a single row will enter into the right dir loop and upon
        exiting the loop will check the length of result against the matrix size. It will then return the row elements.
      - For a column, it will go right once, then go down and after that it will do a
        length check and exit the loop by returning the result.
    */
    
    /* =========================== MY SOLUTION ===========================
    
      var spiralOrder = function(matrix) {
        let rowMin = 0;
        let colMin = 0;
        let rowMax = matrix.length - 1;
        let colMax = matrix[0].length - 1;
        let matrixSize = matrix.length * matrix[0].length;
        let result = [];
        let row = rowMin;
        let col = colMin;
    
        while (result.length < matrixSize) {
            for (col = colMin; col <= colMax; col++) {
                result.push(matrix[row][col]);
            }
            if (result.length === matrixSize) {
                    return result;
                }
            col--;
            rowMin++;
    
            for (row = rowMin; row <= rowMax; row++) {
                result.push(matrix[row][col]);
            }
            if (result.length === matrixSize) {
                   return result;
                }
            row--;
            colMax--;
    
            for (col = colMax; col >= colMin; col--) {
                result.push(matrix[row][col]);
            }
            if (result.length === matrixSize) {
                   return result;
                }
            col++;
            rowMax--;
    
            for (row = rowMax; row >= rowMin; row--) {
                result.push(matrix[row][col]);
            }
            if (result.length === matrixSize) {
                    return result;
                }
            row++;
            colMin++;
        }
        return result;
      };
    
    */

---
+++ 54_SpiralMatrix.js
@@ -0,0 +1,99 @@
+/* Given an m x n matrix, return all elements of the matrix in spiral order.
+
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [1,2,3,6,9,8,7,4,5]
+
+    Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+    Constraints:
+      m == matrix.length
+      n == matrix[i].length
+      1 <= m, n <= 10
+      -100 <= matrix[i][j] <= 100
+*/
+
+/* =========================== MY APPROACH ===========================
+
+Since it's a matrix, I can assume it has no more than 4 corners.
+Since it's asking me to go in spiral order, I will assume the direction to be clockwise. That means it's going right, down, left, up.
+
+So I need my code to read the input in right, down, left, up directions on a loop until it has reached the end.
+
+Initializing some boundaries such rowMin = 0 and colMin = 0,rowMax = m - 1, colMax = n - 1 will help me shrink the size of the
+matrix so I can continue to spiral inwards.
+
+right ---> staying at rowMin, iterate from colMin - colMax (rowMin++)
+down --->  staying at colMax, iterate from rowMin - rowMax (colMax--)
+left --->  staying at rowMax, iterate from colMax - colMin (rowMax--)
+up --->  staying at colMin, iterate from rowMax - rowMin (colMin++)
+
+How will I know that I have hit the end of the spiral?
+  - Well m * n gives us the exact number of elements that should be there in this matrix. So as we spiral inwards pushing elements into our result, we can check if the length of the result = size of the matrix.
+  - So our stopping condition would be when the length of the result is equal to the size of the matrix.
+
+What if my array is []?
+  - I can return null or anything that is required of that edge case
+
+Will this work for single row or column?
+  - It should because a single row will enter into the right dir loop and upon
+    exiting the loop will check the length of result against the matrix size. It will then return the row elements.
+  - For a column, it will go right once, then go down and after that it will do a
+    length check and exit the loop by returning the result.
+*/
+
+/* =========================== MY SOLUTION ===========================
+
+  var spiralOrder = function(matrix) {
+    let rowMin = 0;
+    let colMin = 0;
+    let rowMax = matrix.length - 1;
+    let colMax = matrix[0].length - 1;
+    let matrixSize = matrix.length * matrix[0].length;
+    let result = [];
+    let row = rowMin;
+    let col = colMin;
+
+    while (result.length < matrixSize) {
+        for (col = colMin; col <= colMax; col++) {
+            result.push(matrix[row][col]);
+        }
+        if (result.length === matrixSize) {
+                return result;
+            }
+        col--;
+        rowMin++;
+
+        for (row = rowMin; row <= rowMax; row++) {
+            result.push(matrix[row][col]);
+        }
+        if (result.length === matrixSize) {
+               return result;
+            }
+        row--;
+        colMax--;
+
+        for (col = colMax; col >= colMin; col--) {
+            result.push(matrix[row][col]);
+        }
+        if (result.length === matrixSize) {
+               return result;
+            }
+        col++;
+        rowMax--;
+
+        for (row = rowMax; row >= rowMin; row--) {
+            result.push(matrix[row][col]);
+        }
+        if (result.length === matrixSize) {
+                return result;
+            }
+        row++;
+        colMin++;
+    }
+    return result;
+  };
+
+*/
+
+
--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # My-Leetcode-Practice-Solutions
-Technical Interview Prep
-work!
+
+Compiling all the leetcode problems I have practiced so far, with an explanation of
+my approach and my coded solution.
+
+I try my best to optimize time and space and as such AVOID taking shortcuts or using built-in methods if they add unnecessary time or modify the original data.
+
+I got introduced to algorithms this year (2022). At first I felt lost because I jumped into Leetcode without really spending any time on the fundamentals. Having a strong background in mechanical engineering definitely helped with understanding what a problem was asking for and how to tackle it but translating that into an efficient javascript solution is a journey that cannot be rushed or mastered in a day. But if I can do it, anyone can.
+
+There was a time when I was clueless on easy problems or had the most brute solution. But with practice, it became easier and easier. Some of the folks I chatted with that got into Google have solved around 600 - 900 leetcode problems. So you know that practice definitely helps.
+
+So when you read my solutions, do note that it's the culmination of several hours of understanding the fundamentals of data structures first through several different resources.
+
+Online resources:
+Udemy, Codecademy,YouTube, Structy, Educative, LinkedIn Learning, LeetCode
+
+Books:
+Cracking the coding interview
+
+I jumped back and forth between understanding a data structure and then solving problems pertaining to it. Then moving on to another data structure. It's easy to get demotivated when you come across a problem that's hard to even understand. But that's when I started looking up example applications of data structures and that made the problems seem familiar and less daunting.
+
+Also, never try to memorize solutions. I used to memorize math and physics formulas as a kid. It's easy. But algorithms are far more than that. They teach you how to reason and recognize patterns. I honestly am enjoying this learning process. It's like solving mini puzzles for fun.
+
+Enough said, time to code!
+
